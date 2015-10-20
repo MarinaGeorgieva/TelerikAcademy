9ba5e9d4b27b6e76e65146a217cdc6a2ede443c8
MarinaGeorgieva <marina.iv.georgieva@gmail.com>
2015-10-20 20:48:50 +0300

    Implemented tasks 6 and 7

---
+++ Databases/ADO.NET/ADO.NET-Homework/ReadFromExcelFileAndDisplayData/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ReadFromExcelFileAndDisplayData
+{
+    using System;
+    using System.Data.OleDb;
+
+    public class Program
+    {
+        private const string ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;" +
+            @"Data Source=../../trainers.xlsx;Extended Properties=Excel 12.0 Xml;";
+
+        public static void Main()
+        {
+            using (var dbConnection = new OleDbConnection(ConnectionString))
+            {
+                dbConnection.Open();
+                var excelSchema = dbConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+                string sheetName = excelSchema.Rows[0]["TABLE_NAME"].ToString();
+                string commandText = "SELECT * FROM [" + sheetName + "]";
+                var command = new OleDbCommand(commandText, dbConnection);
+
+                using (OleDbDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string name = reader["Name"].ToString();
+                        double score = (double)reader["Score"];
+                        Console.WriteLine("{0} - {1}", name, score);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Databases/ADO.NET/ADO.NET-Homework/WriteDataToExcelFile/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace WriteDataToExcelFile
+{
+    using System;
+    using System.Data.OleDb;
+
+    public class Program
+    {
+        private const string ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;" +
+           @"Data Source=../../trainers.xlsx;Extended Properties=Excel 12.0 Xml;";
+
+        private const string NameToAdd = "Peter Petrov";
+        private const double ScoreToAdd = 21;
+
+        public static void Main()
+        {
+            using (var dbConnection = new OleDbConnection(ConnectionString))
+            {
+                dbConnection.Open();
+
+                InsertNewRowToExcelFile(dbConnection, NameToAdd, ScoreToAdd);                
+            }
+        }
+        private static void InsertNewRowToExcelFile(OleDbConnection dbConnection, string name, double score)
+        {
+            var excelSchema = dbConnection.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+            string sheetName = excelSchema.Rows[0]["TABLE_NAME"].ToString();
+            string commandText = "INSERT INTO [" + sheetName + "] (Name, Score) VALUES (@name, @score)";
+            var command = new OleDbCommand(commandText, dbConnection);
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@score", score);
+
+            var queryResult = command.ExecuteNonQuery();
+            Console.WriteLine("{0} rows inserted successfully", queryResult);        
+        }
+    }
+}
