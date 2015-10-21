bf2e281c7ed483bdf478c0342503a6cae15ea7fd
MarinaGeorgieva <marina.iv.georgieva@gmail.com>
2015-10-21 13:43:58 +0300

    Added task 5 to ADO.NET Homework

---
+++ Databases/ADO.NET/ADO.NET-Homework/RetrieveImagesForAllCategoriesAsJpgFiles/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace RetrieveImagesForAllCategoriesAsJpgFiles
+{
+    using System.Configuration;
+    using System.Data.SqlClient;
+    using System.IO;
+
+    public class Program
+    {
+        private static SqlConnection dbConnection;
+
+        public static void Main()
+        {
+            ConnectToDB();
+            using (dbConnection)
+            {
+                RetrieveCategoriesImages(dbConnection);
+            }
+        }
+
+        private static void RetrieveCategoriesImages(SqlConnection dbConnection)
+        {
+            string commandText = "SELECT CategoryName, Picture FROM Categories";
+            var command = new SqlCommand(commandText, dbConnection);
+            var reader = command.ExecuteReader();
+            using (reader)
+            {
+                while (reader.Read())
+                {
+                    string categoryName = (string)reader["CategoryName"];
+                    categoryName = categoryName.Replace('/', '-');
+                    byte[] image = (byte[])reader["Picture"];
+                    string fileName = string.Format("../../{0}.jpg", categoryName);
+                    StoreImageAsJpgFile(fileName, 78, image);
+                }
+            }
+        }
+
+        private static void StoreImageAsJpgFile(string fileName, int offset, byte[] image)
+        {
+            FileStream stream = File.OpenWrite(fileName);
+            using (stream)
+            {
+                stream.Write(image, offset, image.Length - offset);
+            }
+        }
+
+        private static void ConnectToDB()
+        {
+            ConnectionStringSettings dbConnectionString = ConfigurationManager.ConnectionStrings["SQLDB"];
+            dbConnection = new SqlConnection(dbConnectionString.ConnectionString);
+            dbConnection.Open();
+        }
+    }
+}
