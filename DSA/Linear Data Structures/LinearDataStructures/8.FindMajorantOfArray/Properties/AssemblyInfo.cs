ada3e9a500d1b0071b26453a2c6e5f88cb7c1262
MarinaGeorgieva <marina.iv.georgieva@gmail.com>
2015-10-29 18:30:38 +0200

    Added task 8

---
+++ DSA/Linear Data Structures/LinearDataStructures/8.FindMajorantOfArray/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("8.FindMajorantOfArray")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("8.FindMajorantOfArray")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("61b2a16f-d032-43fb-9a96-b2f54512db4d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DSA/Linear Data Structures/LinearDataStructures/8.FindMajorantOfArray/Startup.cs
@@ -0,0 +1,52 @@
+﻿namespace FindMajorantOfArray
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            List<int> array = new List<int> { 2, 2, 3, 3, 2, 3, 4, 3, 3 };
+            int minOccurrences = array.Count / 2 + 1;
+
+            SortedDictionary<int, int> elementsCount = new SortedDictionary<int, int>();
+            int index = 0;
+            while (index < array.Count)
+            {
+                int element = array[index];
+                if (elementsCount.ContainsKey(element))
+                {
+                    elementsCount[element]++;
+                }
+                else
+                {
+                    elementsCount.Add(element, 1);
+                }
+
+                index++;
+            }
+
+            int maxCount = 1;
+            int majorant = elementsCount.Keys.First();
+            foreach (var pair in elementsCount)
+            {
+                if (pair.Value > maxCount)
+                {
+                    maxCount = pair.Value;
+                    majorant = pair.Key;
+                }
+            }
+
+            if (maxCount >= minOccurrences)
+            {
+                Console.WriteLine("The majorant is {0}", majorant);
+            }
+            else
+            {
+                Console.WriteLine("The given array does not have a majorant!");
+            }
+        }
+    }
+}
