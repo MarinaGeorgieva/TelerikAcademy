15e00882b92443e1207f196efafeed7e4bcac965
MarinaGeorgieva <marina.iv.georgieva@gmail.com>
2015-11-10 10:03:25 +0200

    Added Consuming Web Services using C# Homework

---
+++ Web Services and Cloud/Consuming Web Services using C#/ConsumingWebServices/RetrievePosts/Post.cs
@@ -0,0 +1,22 @@
+﻿namespace RetrievePosts
+{
+    public class Post
+    {
+        public int UserId { get; set; }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Body { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format(
+                "Id: {0} \nTitle: {1} \nBody: {2}\n", 
+                this.Id, 
+                this.Title, 
+                this.Body);
+        }
+    }
+}
---
+++ Web Services and Cloud/Consuming Web Services using C#/ConsumingWebServices/RetrievePosts/Startup.cs
@@ -0,0 +1,57 @@
+﻿namespace RetrievePosts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+
+    using Newtonsoft.Json;
+
+    public class Startup
+    {
+        private const string Url = @"http://jsonplaceholder.typicode.com/";
+
+        public static void Main()
+        {
+            Console.Write("Enter query string (by default set to 'dolorem'): ");
+            string queryString = Console.ReadLine();
+            if (string.IsNullOrEmpty(queryString))
+            {
+                queryString = "dolorem";
+            }
+
+            Console.Write("Enter count of posts to retrieve (by default set to 3): ");
+            int count;
+            bool isValidNumber = int.TryParse(Console.ReadLine(), out count);
+            if (!isValidNumber)
+            {
+                count = 3;
+            }
+
+            Console.WriteLine("Retrieving posts...");
+            GetPostsByQueryString(queryString, count);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+
+        private static async void GetPostsByQueryString(string query, int count)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(Url);
+                var response = await httpClient.GetAsync("posts");
+
+                var content = response.Content.ReadAsStringAsync().Result;
+                var posts = JsonConvert.DeserializeObject<List<Post>>(content);
+                var filteredPosts = posts.Where(p => p.Title.Contains(query)).Take(count);
+
+                Console.WriteLine("\nPosts: \n");
+                foreach (var post in filteredPosts)
+                {
+                    Console.WriteLine(post);
+                }
+            }            
+        }
+    }
+}
